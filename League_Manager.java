e4d7aee5e2178e591835d50b5a12da24c4545a8e
mcnabola <35931286+mcnabola@users.noreply.github.com>
2018-03-03 01:20:05 +0000

    Seans Code

--- League_Manager.java
+++ League_Manager.java
@@ -1,173 +1,20 @@
-// Paul's code so far.Got the gui roughlygoing.Only made up mock versions of methods to make sure it works.
-import java.io.FileOutputStream;
-import java.io.OutputStreamWriter;
-import java.io.Writer;
-import java.io.BufferedWriter;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import javax.swing.JOptionPane;
-import java.util.Arrays;
+//seans code so far
+import java.io.*;
+import javax.swing.*;
 import java.util.*;
-public class gui
+
+public class League
 {
-	public static void main(String[]args)
-	{   
-        String initialOptions= " 1-create league \n 2-Edit/view League \n 3-Remove League \n 4-Exit Application ";
-		String subOptions=" 1-Fixture Generation \n 2-View Table 3-Input results \n 4-Add/remove teams \n 5-number of leagues made";
-		String subOptionsOfSubOptions=" 1-Add teams 2-remove teams";
-		
-		
+	private static int currentAdminNo=1;
+	private static int leagueNo=1;
+	final static String leagueFile="league.txt";
+	final static String adminFile="administrator.txt";
 	
-		//if (logIn("Enter your username and password"))=true; // thomas method
-		//{
-			int x=menuBoxInt(initialOptions);
-			if (x== 1)
-			{
-				createNewLeague();
-			}
-			if (x==2)
-			{
-				int y=menuBoxInt(subOptions);
-				if (y==1)
-				{
-					String r=menuBox("genfix");
-					generateFixtures(r);
-				}
-				if (y==2)
-				{
-					String v=menuBox("gentable");
-					generateTable(v);
-				}
-				if (y==3)
-				{
-					String r=menuBox("displayTable");
-					displayTable(r);
-				}
-				if (y==4)
-				{
-					int z=menuBoxInt(subOptionsOfSubOptions);
-					if (z==1)
-					{
-					 String r=menuBox("addTeamsToLeague");
-					 addTeamsToLeague(r);	
-					}
-					if (z==2)
-					{
-						String r=menuBox("addTeamsToLeague");
-						removeTeamsFromLeague(r);
-					}
-				}
-				if (y==5)
-				{
-					getNumberOfLeaguesMade();
-				}
-			}
-			
-			if (x==3)
-			{
-				String r=menuBox("addTeamsToLeague");
-				removeLeague(r);
-			}
-			if (x==4)
-			{
-				System.exit(0);
-			}
-			
-				else 
-				{
-					outputBoxs("Number outside 1-5");
-				}
-		}
-			
-			
-		//}
-	
-	
-	public static int menuBoxInt(String options)
-	{   int x=-1;
-		String text = JOptionPane.showInputDialog(null, options);
-		boolean valid = validateNumberInput(text); 
-		if (valid=true)
-		{
-		// parse to int and return the users choice
-		 x = Integer.parseInt(text);
-		}
-		return x;
-	}
-	
-	public static boolean validateNumberInput(String text)
+	public static void main(String [] args)
 	{
-		String result="";
-		String pattern="[1-4]{1}";
-		String message1 = "A number must be input.";
-	    String message2 = "Format of user input is incorrect, a number  is required.";
-		boolean verified=false;
-		if      (text != null)        
-		{ 
-			if (text.equals(""))   
-			{
-				result = message1;
-				outputBoxs(message1);
-			}
-			else
-			{				
-		
-				if (text.indexOf(" ") != -1) text = text.replaceAll("\\s+","");
-	        
-					if (!text.matches(pattern)) 
-					{
-					result = message2;
-					outputBoxs(message2);
-					verified=true;
-					}
-			}
-		}
-		return verified ;
+	createNewLeague();
+    addTeamsToLeague();
 	}
-	
-				
-	public static boolean validateInput(String text)
-	{
-		String result="";
-		String pattern="[A-Za-z||0-9]{1,}";
-		String message1 = "A Name must be input.";
-	    String message2 = "Format of user input is incorrect, a Name is required.";
-		boolean verified=false;
-		if      (text != null)        
-		{ 
-			if (text.equals(""))   
-			{
-				result = message1;
-				outputBoxs(message1);
-			}
-			else
-			{				
-		
-				if (text.indexOf(" ") != -1) text = text.replaceAll("\\s+","");
-	        
-					if (!text.matches(pattern)) 
-					{
-					result = message2;
-					outputBoxs(message2);
-					verified=true;
-					}
-			}
-		}
-		return verified ;
-	}
-        private static int currentAdminNo=1;
-		private static int leagueNo=1;
-		final static String leagueFile="league.txt";
-		final static String adminFile="administrator.txt";
-	
-	public static void createNewLeague()
-	{
-		String leagueName=""; String leagueFileInput="";
-		leagueName=menuBox("Enter your league name:");
-		leagueFileInput=currentAdminNo+","+leagueName+","+leagueNo;
-		writeFile(leagueFileInput,leagueFile);
-		leagueNo++;
-				}
 	
 		 
 	public static void writeFile(String input, String fileName) 
@@ -189,8 +36,21 @@ public class gui
 		 return input;
 	 }
 	 
+	 public static int menuBoxInt(String options)
+	{
+		String text = JOptionPane.showInputDialog(null, options);
+		int x = Integer.parseInt(text);
+		return x;
+	}
 	
-	
+	public static void createNewLeague()
+	{
+		String leagueName=""; String leagueFileInput="";
+		leagueName=menuBox("Enter your league name:");
+		leagueFileInput=currentAdminNo+","+leagueName+","+leagueNo;
+		writeFile(leagueFileInput,leagueFile);
+	    leagueNo++;
+	}
 	
 	public static int getNumberOfLeaguesMade()
 	{
@@ -265,45 +125,9 @@ public class gui
 				info="enter team/player number:"+(i+1);
 				teamName=menuBox(info);
 				teamFileInfo=(i+1)+","+teamName;
-				writeFile(teamFileInfo,teamFileName);	
-        }
-
+				writeFile(teamFileInfo,teamFileName);
+			}
 		}
 	}
-	public static String createNewLeague(String x)
-	{
-		outputBoxs(x);
-		return x;
-	}
-	
-	public static String generateFixtures(String x)
-	{
-		outputBoxs(x);
-		return x;
-	}
-	public static String generateTable(String x)
-	{
-		outputBoxs(x);
-		return x;
-	}
-	public static String removeTeamsFromLeague(String x)
-	{
-		outputBoxs(x);
-		return x;
-	}
-	public static String removeLeague(String x)
-	{
-		outputBoxs(x);
-		return x;
-	}
-	public static String addTeamsToLeague(String x)
-	{
-		outputBoxs(x);
-		return x;
-	}
-	public static String displayTable(String x)
-	{
-		outputBoxs(x);
-		return x;
-	}
+
 }
