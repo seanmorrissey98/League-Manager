f7f32e9863939e74a2bb6a4745f2fa26bb76d000
seanmorrissey98 <35046602+seanmorrissey98@users.noreply.github.com>
2018-03-05 10:58:59 +0000

    Update League_Manager.java

--- League_Manager.java
+++ League_Manager.java
@@ -1,34 +1,62 @@
-//seans code so far
 import java.io.*;
 import javax.swing.*;
 import java.util.*;
-
 public class League
 {
 	private static int currentAdminNo=1;
 	private static int leagueNo=1;
 	final static String leagueFile="league.txt";
 	final static String adminFile="administrator.txt";
+	private static String item1;
 	
 	public static void main(String [] args)
 	{
+	//createNewLeague();
+    //addTeamsToLeague();
+	//fixtureGeneration();
+	String username = JOptionPane.showInputDialog(null, "Enter username");
+	String password = JOptionPane.showInputDialog(null, "Enter password"); //Will work on method for hidden password input in swing
+	boolean isLoggedIn = loginMethod(username, password);
 	createNewLeague();
     addTeamsToLeague();
+	fixtureGeneration();
 	}
 	
-		 
-	public static void writeFile(String input, String fileName) 
-	 {
-         try (Writer writer = new BufferedWriter(new OutputStreamWriter(
-         new FileOutputStream(fileName), "utf-8"))) 
-		 {
-             writer.write(input);
-         }
-         catch(Exception e)
-         {} 
-         
-	 }
-	 
+		public static void writeFile(String input, String fileName)
+		{
+		try
+		{
+		    FileWriter aFileWriter = new FileWriter(fileName,true);
+            PrintWriter out = new PrintWriter(aFileWriter);
+			out.print(input);
+			out.println();
+			out.close();
+			aFileWriter.close();	
+		}
+		catch(Exception e)
+		{}
+		}
+			
+	public static String readFile(String textFile)
+	{
+		String fileText="";
+		try
+		{
+		FileReader reader=new FileReader(textFile);
+		Scanner in=new Scanner(reader);
+		while(in.hasNext())
+		{
+			fileText=in.nextLine();
+			fileText=fileText+",";
+		}
+		in.close();
+		reader.close();
+		}
+		catch(Exception E)
+		{}
+		return fileText;
+	}
+	
 	 public static String menuBox(String Options)
 	 {
 		 String input="";
@@ -43,9 +71,20 @@ public class League
 		return x;
 	}
 	
+	public static void outputBoxs(String output)
+	{
+	     JOptionPane.showMessageDialog(null, output);	
+	}
+	
+	public static void outputBoxs(int output)
+	{
+		JOptionPane.showMessageDialog(null,output);
+	}
+	
 	public static void createNewLeague()
 	{
-		String leagueName=""; String leagueFileInput="";
+		String leagueName=""; 
+		String leagueFileInput="";
 		leagueName=menuBox("Enter your league name:");
 		leagueFileInput=currentAdminNo+","+leagueName+","+leagueNo;
 		writeFile(leagueFileInput,leagueFile);
@@ -83,39 +122,17 @@ public class League
 		return numberOfLeagues;
 	}
 	
-	public static String readFile(String textFile)
-	{
-		String fileText="";
-		try
-		{
-		FileReader reader=new FileReader(textFile);
-		Scanner in=new Scanner(reader);
-		while(in.hasNext())
-		{
-			fileText=in.nextLine();
-			fileText=fileText+",";
-		}
-		in.close();
-		reader.close();
-		}
-		catch(Exception E)
-		{}
-		return fileText;
-	}
-	
-	public static void outputBoxs(String output)
-	{
-	     JOptionPane.showMessageDialog(null, output);	
-	}
-	
 	public static void addTeamsToLeague()
 	{
-		String info=""; String teamName=""; String teamFileInfo=""; String teamFileName="";
+		String info=""; 
+		String teamName=""; 
+		String teamFileInfo=""; 
+		String teamFileName="";
 		int whichLeague=menuBoxInt("Enter which league number to add teams/players to:");
-		teamFileName=whichLeague+"_"+"participants.txt";
+		teamFileName=whichLeague+"_participants.txt";
 		if(getNumberOfLeaguesMade()<whichLeague)
 		{
-			outputBoxs("This league does not exist.");
+			outputBoxs("This league does not exist or you have not created it.");
 		}
 		else
 		{
@@ -129,5 +146,190 @@ public class League
 			}
 		}
 	}
+	
+	public static void fixtureGeneration()
+	{
+		String info=""; 
+		String fixtureFileInfo=""; 
+		String fixtureGenerationFileName="";
+		int numberOfTeams=0;
+		String teamFileName="";
+		int totalNumberOfRounds=0;
+		int numberOfMatchesPerRound=0;
+		int roundNumber=0;
+		int matchNumber=0;
+		int homeTeamNumber=0;
+		int awayTeamNumber=0;
+		int even=0;
+		int odd=0;
+		boolean additionalTeamIncluded=false;
+		String [][] fixtures;
+		String [][]revisedFixtures;
+		String [] elementOfFixture;
+		String fixtureAsText="";
+		int whichLeague=menuBoxInt("Enter which league number to generate fixtures for:");
+		fixtureGenerationFileName=whichLeague+"_"+"fixtures.txt";
+		teamFileName=whichLeague+"_participants.txt";
+		if(getNumberOfLeaguesMade()<whichLeague)
+		{
+			outputBoxs("You did not create this league, access denied.");
+		}
+		else
+		{
+			numberOfTeams=getNumberOfTeams(teamFileName);
+			if (numberOfTeams%2==1)
+			{
+				numberOfTeams++;
+				additionalTeamIncluded=true;
+			}
+			totalNumberOfRounds=numberOfTeams-1;
+			numberOfMatchesPerRound=numberOfTeams/2;
+			fixtures=new String[totalNumberOfRounds][numberOfMatchesPerRound];
+			for(roundNumber=0; roundNumber<totalNumberOfRounds;roundNumber++)
+			{
+				for(matchNumber=0;matchNumber<numberOfMatchesPerRound;matchNumber++)
+				{
+					homeTeamNumber=(roundNumber+matchNumber)%(numberOfTeams-1);
+					awayTeamNumber=(numberOfTeams-1-matchNumber+roundNumber)%(numberOfTeams-1);
+					if(matchNumber==0)
+						awayTeamNumber=numberOfTeams-1;
+					fixtures[roundNumber][matchNumber]=(homeTeamNumber+1)+"v"+(awayTeamNumber+1);
+				}
+			}
+			revisedFixtures=new String[totalNumberOfRounds][numberOfMatchesPerRound];
+			even=0;
+			odd=numberOfTeams/2;
+			for(int i=0;i<fixtures.length;i++)
+			{
+				if(i%2==0)
+					revisedFixtures[i]=fixtures[even++];
+				else
+			revisedFixtures[i]=fixtures[odd++];
+			}
+			fixtures=revisedFixtures;
+			for(roundNumber=0;roundNumber<fixtures.length;roundNumber++)
+			{
+				if(roundNumber%2==1)
+				{
+					fixtureAsText=fixtures[roundNumber][0];
+					elementOfFixture=fixtureAsText.split("v");
+					fixtures[roundNumber][0]=elementOfFixture[1]+"v"+elementOfFixture[0];
+				}
+			}
+			int matchCounter=1;
+			for(roundNumber=0;roundNumber<totalNumberOfRounds;roundNumber++)
+			{
+				for(matchNumber=0;matchNumber<numberOfMatchesPerRound;matchNumber++)
+				{
+					info=matchCounter+",";
+					info=info+fixtures[roundNumber][matchNumber].substring(0,1)+","+fixtures[roundNumber][matchNumber].substring(2,3);
+					writeFile(info,fixtureGenerationFileName);
+					matchCounter++;
+				}
+			}
+		}
+	}
+	
+	public static int getNumberOfTeams(String teamFileName)
+	{
+		int numberOfTeams=0;
+		String [] arrayOfDetails=readFile(teamFileName).split(",");
+		numberOfTeams=Integer.parseInt(arrayOfDetails[arrayOfDetails.length - 2]);
+		return numberOfTeams;
+	}
+	
+	public static String getTeamName(int teamNumber, String teamFileName)
+	{
+		String teamName="";
+		int arrayNum=0;
+		int positionInArray=1;
+		String [] arrayOfDetails=readFile(teamFileName).split(",");
+		for(int i=0;i<arrayOfDetails.length;i++)
+		{
+			if(i%2==0)
+			{
+				arrayNum=Integer.parseInt(arrayOfDetails[i]);
+				if(teamNumber==arrayNum)
+					positionInArray=i+1;
+			}	
+		}	
+		teamName=arrayOfDetails[positionInArray];
+		return teamName;
+	}
+
+	/*public static void main (String [] args)
+	{ 	
+		
+		String username = JOptionPane.showInputDialog(null, "Enter username");
+		String password = JOptionPane.showInputDialog(null, "Enter password"); //Will work on method for hidden password input in swing
+		boolean isLoggedIn = loginMethod(username, password);
+		if (isLoggedIn == true)
+		{
+			//Pauls Menu methods 
+		}
+	}*/
+	
+	
+	public static boolean loginMethod(String username, String password)
+	{
+		String loginMessage = "";
+		int maxLoginAttempts = 2; 
+		boolean loggedInStatus = false;
+		boolean foundUsername = readFile("Administrators.txt", username, 1);
+		boolean foundPassword = readFile("Administrators.txt", password, 2);
+		
+		while (maxLoginAttempts > 0)
+		{
+			if (foundUsername == true && foundPassword == true)
+			{
+				loggedInStatus = true;
+				loginMessage = "Successfully logged in as " + username;
+				JOptionPane.showMessageDialog(null, loginMessage);
+				break;
+			}
+			else
+			{
+				loginMessage = "Incorrect login details\n" + maxLoginAttempts + " attempt(s) remaining";
+				JOptionPane.showMessageDialog(null, loginMessage);
+				maxLoginAttempts--;
+				username = JOptionPane.showInputDialog(null, "Enter username");
+				password = JOptionPane.showInputDialog(null, "Enter password");
+				if (maxLoginAttempts == 0)
+				{
+					loginMessage = "Invalid login details\nNo login attempts remaining";
+					JOptionPane.showMessageDialog(null, loginMessage);
+				}
+			}
+		}
+	return loggedInStatus;
+	}
+	
+	public static Boolean readFile(String textFile, String searchedItem, int itemPositionNo)
+    	{
+		boolean found = false;
+		try
+		 {
+			item1 = "";
+	        FileReader reader=new FileReader(textFile);
+			Scanner in=new Scanner(reader);
+			while(in.hasNext())
+			{    
+		        String fileText=in.nextLine();
+		        String[] split = fileText.split(",");
+				if (split[itemPositionNo].equals(searchedItem))
+				{
+					found = true;
+					item1 = split[0]; // Admin#, League#, fixture#.
+					break;
+				}
+			}
+			in.close();
+			reader.close();	
+		 }
+		 catch (Exception e)
+		 {}
+		
+		return found;
+	}	
 
 }
